663646b9393aba32218b72bcd8a301e2a9a7ec0d
Josiah Storrie <jpstorrie@gmail.com>
2023-02-13 13:53:46 -0700

    started building forums.jsx

--- client-phase-3-project/src/components/Forums.jsx
+++ client-phase-3-project/src/components/Forums.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react"
 
 function Forums(){
-    return
+
+    const [formQuestions, setFormQuestions] = useState([])
+
+
+    // Fetch all questions and set the state of formQuestions
+    useEffect(()=> {
+        fetch("/api/forums")
+        .then(r => r.json())
+        .then(data => setFormQuestions(data))
+    }, [])
+
+
+    // Iterate through all the questions and display the question
+    // and its details
+    const questionIterations = formQuestions.forEach(question =>
+        <div>
+            <h3>{question.question}</h3>
+            <h5>{question.username}</h5>
+            <p>{question.date}</p>
+        </div>
+    )
+
+    // Forum.create(game_id: Game.first.id,username: "Pickles", question: "Why mirage sad?", date: DateTime.now)
+
+    return (
+        <div>
+        <h1>Forums</h1>
+        {questionIterations}
+        </div>
+    )
 }
 export default Forums
