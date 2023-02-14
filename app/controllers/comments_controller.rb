c9dcf6c5d388bb11c39d926c9c7ce83a5cfc78c0
Josiah Storrie <jpstorrie@gmail.com>
2023-02-14 10:44:30 -0700

    added navbar link to forums and support

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
 
     def patch_like
+        #get the comment
         comment = Comment.find(params[:id])
-        comment.likes += 1
+        #set a variable to update the like count
+        updated_like = comment.likes += 1
+        #update using the variable
+        comment.update(likes: updated_like)
         render json: [comment.likes]
     end
 end
