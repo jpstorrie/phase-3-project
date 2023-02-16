fdc791e60ff258ec88b066b3ec818c785c0c9e16
thomas <tom.angotti11@gmail.com>
2023-02-16 14:54:48 -0700

    added genre lists

--- client-phase-3-project/src/components/Games.jsx
+++ client-phase-3-project/src/components/Games.jsx
@@ -22,8 +22,112 @@ function Games() {
       return game.name.toLowerCase().includes(search.toLowerCase())
     })
 
-    const gameList =filteredGames.map((game) => {
+    const gameList = filteredGames.map((game) => {
       return <RenderGamesCard key={game.id} game={game}/>
+    })
+    ////////////////////
+    /// action game list
+    //////////////////// 
+    const actionGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("action")
+    })
+
+    const actionList = actionGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+    ///////////////////////
+    /// adventure game list
+    ///////////////////////
+    const adventureGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("adventure")
+    })
+
+    const adventureList = adventureGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+
+    ///////////////
+    //fps game list
+    ///////////////
+
+    const fpsGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("fps")
+    })
+
+    const fpsList = fpsGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+
+    //////////////////////
+    //open world game list
+    //////////////////////
+
+    const owGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("open_world")
+    })
+
+    const owList = owGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+
+    //////////////////////
+    //rpg game list
+    //////////////////////
+
+    const rpgGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("rpg")
+    })
+
+    const rpgList = rpgGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+
+    //////////////////////
+    //Turn based list
+    //////////////////////
+
+    const tbGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("turn_based")
+    })
+
+    const tbList = tbGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+
+    //////////////////////
+    //horror list
+    //////////////////////
+
+    const horrorGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("horror")
+    })
+
+    const horrorList = horrorGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+
+    //////////////////////
+    //multiplayer list
+    //////////////////////
+
+    const multiGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("multiplayer")
+    })
+
+    const multiList = multiGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
+    })
+
+    //////////////////////
+    //rts list
+    //////////////////////
+
+    const rtsGames = filteredGames.filter((game) => {
+      return game.genre.toLowerCase().includes("rts")
+    })
+
+    const rtsList = rtsGames.map((game) => {
+      return <RenderGamesCard key={game.id} game={game} />
     })
 
   return (
@@ -31,12 +135,29 @@ function Games() {
       <h1>GAMES GAMES GAMES</h1>
       <NavLink
       to="/game/new">
-      <button>ADD GAME</button><br></br>
+      <button>ADD GAME</button>
       </NavLink>
-      <label>Search</label>
+      <label style={{margin: "5px 5px 5px 50px"}}>Search</label>
       <input type="text" onChange={handleChange}></input>
       <br></br>
-      {gameList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>ACTION</h2>
+      {actionList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>ADVENTURE</h2>
+      {adventureList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>FPS</h2>
+      {fpsList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>OPEN WORLD</h2>
+      {owList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>RPG</h2>
+      {rpgList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>TURN-BASED</h2>
+      {tbList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>HORROR</h2>
+      {horrorList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>MULTIPLAYER</h2>
+      {multiList}
+      <h2 style={{margin: "15px 10px 10px 10px"}}>RTS</h2>
+      {rtsList}
     </>
   )
 }
